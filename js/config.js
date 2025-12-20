e5d8b7af371b0dbc6d81e658fcfca989b390a953
HeresJhony <baida9293@gmail.com>
2025-12-20 16:02:49 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZiZ3Jsenh6bmNreWR6dnR5YnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNzczMTAsImV4cCI6MjA4MDg1MzMxMH0.inIcf6aQfgOzGCt4ubceG61AdD0go59AGUxCz-zaw3c',
     // Google Apps Script Deployment URL
     // Google Apps Script Deployment URL
-    ORDER_API_URL: "https://script.google.com/macros/s/AKfycbyAFYFurwkF9bZ2EXRC8Ym7hZcHxi4_5Squ3VE0ZRnegr0NEmB2O73VpBbXTGRDnw/exec",
+    ORDER_API_URL: "https://script.google.com/macros/s/AKfycbx6i-R1gCJMQ-FkNV_267DxH8mDAQ3J4OUpXBSOEmTU8674aqT5R2pFN2q2o-XpvoCLzA/exec",
 };
 
 export const API_HEADERS = {
