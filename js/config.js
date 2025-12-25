35eb68596a197aeeac1be0bf72bc662f821e790d
HeresJhony <baida9293@gmail.com>
2025-12-26 01:43:20 +1000

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
     SUPABASE_URL: 'https://fbgrlzxznckydzvtybvj.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZiZ3Jsenh6bmNreWR6dnR5YnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNzczMTAsImV4cCI6MjA4MDg1MzMxMH0.inIcf6aQfgOzGCt4ubceG61AdD0go59AGUxCz-zaw3c',
     // Google Apps Script Deployment URL
-    ORDER_API_URL: "https://script.google.com/macros/s/AKfycbx9RJBtAi3pvSTqtFMzp4QTFNE77hHyoHrGPY_udKgEBEl6Kk9our3LnPAIqwchf6SD7A/exec",
+    ORDER_API_URL: "https://fbgrlzxznckydzvtybvj.supabase.co/functions/v1/telegram-bot",
 };
 
 export const API_HEADERS = {
